3262fc4ecec61cfa90b4d298cde39b8e08c05ba2
marcothelost <marcothelost@proton.me>
2024-03-21 21:08:10 +0100

    Drawing a square

--- examples/Main.c
+++ examples/Main.c
@@ -9,6 +9,41 @@ const unsigned int WINDOW_WIDTH  = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 const char*        WINDOW_TITLE  = "DarkBrim";
 
+// Settings
+const unsigned int INFO_LOG_SIZE = 512;
+
+// Shader Source Codes
+const char* vertexShaderSource =
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "layout (location = 1) in vec3 aCol;\n"
+  "out vec3 vertCol;\n"
+  "void main()\n"
+  "{\n"
+  "  vertCol = aCol;\n"
+  "  gl_Position = vec4(aPos, 1.f);\n"
+  "}\0";
+const char* fragmentShaderSource =
+  "#version 330 core\n"
+  "in vec3 vertCol;\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "  FragColor = vec4(vertCol, 1.f);\n"
+  "}\0";
+
+// Vertices and Indices
+GLfloat vertices[] = {
+  -0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f,
+   0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f,
+  -0.5f,  0.5f, 0.f, 1.f, 1.f, 1.f,
+   0.5f,  0.5f, 0.f, 1.f, 1.f, 1.f,
+};
+GLuint indices[] = {
+  0, 1, 3,
+  0, 3, 2,
+};
+
 int main()
 {
   // Initializing GLFW
@@ -51,15 +86,94 @@ int main()
   GLclampf alpha = 1.0f;
   glClearColor(red, green, blue, alpha);
 
+  // Shaders
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // Info Log
+  int success = 0;
+  char infoLog[INFO_LOG_SIZE];
+
+  // Validating the Vertex Shader
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, INFO_LOG_SIZE, NULL, infoLog);
+    fprintf(stderr, "Failed to compile the vertex shader!\n");
+    fprintf(stderr, "Error: \n%s\n", infoLog);
+  }
+
+  // Validating the Fragment Shader
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, INFO_LOG_SIZE, NULL, infoLog);
+    fprintf(stderr, "Failed to compile the fragment shader!\n");
+    fprintf(stderr, "Error: \n%s\n", infoLog);
+  }
+
+  // Shader Program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  // Validating the Shader Program
+  glGetProgramiv(fragmentShader, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, INFO_LOG_SIZE, NULL, infoLog);
+    fprintf(stderr, "Failed to link the shader program!\n");
+    fprintf(stderr, "Error: \n%s\n", infoLog);
+  }
+
+  // Deleting the Shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // VAO, VBO, and EBO
+  GLuint VAO;
+  GLuint VBO;
+  GLuint EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
   // Main Loop
   while (!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
     glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, NULL);
     glfwSwapBuffers(window);
   }
 
   // Termination
+  glDeleteProgram(shaderProgram);
   glfwDestroyWindow(window);
   glfwTerminate();
   return EXIT_SUCCESS;
