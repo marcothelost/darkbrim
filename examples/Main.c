f34aa2e8fdf8d7993d17161a4c9581d0cb58b5f9
marcothelost <marcothelost@proton.me>
2024-03-21 20:29:05 +0100

    Creating a GLFW window

--- examples/Main.c
+++ examples/Main.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Window Settings
+const unsigned int WINDOW_WIDTH  = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char*        WINDOW_TITLE  = "DarkBrim";
+
 int main()
 {
+  // Initializing GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+  // GLFw Window
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE,
+    NULL,
+    NULL
+  );
+  if (window == NULL)
+  {
+    fprintf(stderr, "Failed to create a GLFW window!\n");
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Initializing GLEW
+  GLenum glewErr = glewInit();
+  if (glewErr != GLEW_OK)
+  {
+    fprintf(stderr, "Failed to initialize GLEW!\n");
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  // Clear Color
+  GLclampf red   = 0.0f;
+  GLclampf green = 0.0f;
+  GLclampf blue  = 0.0f;
+  GLclampf alpha = 1.0f;
+  glClearColor(red, green, blue, alpha);
+
+  // Main Loop
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  // Termination
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return EXIT_SUCCESS;
 }
